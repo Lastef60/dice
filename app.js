762ff73b88f592c64c887acac34bf5b04325c74f
Lastef60 <97059844+Lastef60@users.noreply.github.com>
2023-04-16 15:41:38 +0200

    add img + start js

--- app.js
+++ app.js
@@ -1,24 +1,58 @@
 //SELECTIONNER LES ELEMENTS DANS LE DOM
-
-
+const newGame = document.getElementById('newgame')
+const rolldice = document.getElementById('diceBtnRoll')
+const hold = document.getElementById('diceBtnHold')
+const diceDiv = document.getElementById('objDice')
+const diceImg = document.getElementById('objDiceImg')
+let scorePlayerOne = document.getElementById('scoreOne')
+let scorePlayerTwo = document.getElementById('scoreTwo')
+let currentOne = document.getElementById('currentOne')
+let currentTwo = document.getElementById('currentTwo)')
+let playerOneP = document.getElementById('playerOneP')
+let playerTwoP = document.getElementById('playerTwoP')
 //PSEUDO CODE
 
+//creation score
+let scores;
+
+//random
+
+
+
 //AU CLIC BOUTON NEWGAME ADDEVENTLISTENER
-//LES CORES SONT REINITIALISES
+newGame.addEventListener('click', function() {
+//console.log ('bouton cliqué');
+//LES SCORES SONT REINITIALISES
+scores = 0;
+scorePlayerOne.innerText = scores; 
+scorePlayerTwo.innerText = scores;
+//UN CERCLE ROUGE INDIQUE QUEL JOUEUR JOUE
+playerOneP.innerText ='PLAYER 1 \ud83d\udd34'
+//PLAYER1 PEUT JOUER MAIS PAS PLAYER2
 
-//AU CLIC BOUTON HOLD
-// - LES POINTS ROUND --> GLOBAL
-// - TOUR DE L AUTRE JOUEUR changement de couleur de fonds + point rouge a modifier a cote de "joueur"
+});
 
-//AU CLIC BOUTON  ROLLEDICE
-// - UN NOMBRE ALLEATOIRE EST GENERE PAR LE DE MATHFLOORRANDOM + switch pour affichage face du dé
-// - UNE ANIMATION DU DE EST LANCEE
+//AU CLIC SUR ROLLDICE PAR PLAYER 1
+rolldice.addEventListener('click', function() {
+//LE DE DONNE UNE VALEUR ALEATOIRE ENTRE 1 ET 6
+    let min= 1; 
+    let max= 7; 
+    let random = Math.floor(Math.random()*(max - min) + min); 
+    console.log(random);
+ ;
+    //alert(random);
+    
+//affichage dé /
+  let diceFace = document.createElement("IMG");
+diceFace.setAttribute("src","img/dice"+random+".png")
+document.getElementById('objDiceImg').appendChild(diceFace);
 
-//LORSQUE LE DE DONNE 1
-// - LE SCORE ROUND EST REINITIALISE
-// - TOUR DE L AUTRE JOUEUR : changement de couleur de fonds + point rouge a modifier a cote de "joueur"
+//le score est ajouté à 
 
-//LORSQU 1 JOUEUR GLOBAL = 100
-// UNE NOTIFICATION SE DECLENCHE
-// IL N EST PAS POSSIBLE DE CONTINUER A JOUER
 
+
+});
+
+/*rolldice.addEventListener('click', function() {
+  document.getElementById('objDiceImg').removeChild(diceFace); 
+});*/
