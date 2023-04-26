e3fd2399619211c8bcaf90acec236a84b86db38e
Lastef60 <97059844+Lastef60@users.noreply.github.com>
2023-04-26 11:11:34 +0200

    update javascript

--- app.js
+++ app.js
@@ -1,58 +1,107 @@
-//SELECTIONNER LES ELEMENTS DANS LE DOM
-const newGame = document.getElementById('newgame')
-const rolldice = document.getElementById('diceBtnRoll')
-const hold = document.getElementById('diceBtnHold')
-const diceDiv = document.getElementById('objDice')
-const diceImg = document.getElementById('objDiceImg')
-let scorePlayerOne = document.getElementById('scoreOne')
-let scorePlayerTwo = document.getElementById('scoreTwo')
+const newgameBtn = document.getElementById('newgame')
+const rollBtn = document.getElementById('diceBtnRoll')
+const holdBtn = document.getElementById('diceBtnHold')
+let diceImg = document.getElementById('objDiceImg')
+let diceFace = document.createElement("IMG");
+let globalOne = document.getElementById('scoreOne')
+let globalTwo = document.getElementById('scoreTwo')
 let currentOne = document.getElementById('currentOne')
-let currentTwo = document.getElementById('currentTwo)')
+let currentTwo = document.getElementById('currentTwo')
 let playerOneP = document.getElementById('playerOneP')
 let playerTwoP = document.getElementById('playerTwoP')
-//PSEUDO CODE
+const playerOne = document.getElementsByClassName('playerOne');
+const playerTwo = document.getElementsByClassName('playerTwo');
+let roundOne = 0;
+let roundTwo = 0;
+let scoreGlobalOne = 0;
+let scoreGlobalTwo = 0;
+let active = playerOne;
 
-//creation score
-let scores;
 
-//random
+function scoreinit() {
+  roundOne = 0;
+  roundTwo = 0;
+  scoreGlobalOne = 0;
+  scoreGlobalTwo = 0;
+};
 
-
-
-//AU CLIC BOUTON NEWGAME ADDEVENTLISTENER
-newGame.addEventListener('click', function() {
-//console.log ('bouton cliqué');
-//LES SCORES SONT REINITIALISES
-scores = 0;
-scorePlayerOne.innerText = scores; 
-scorePlayerTwo.innerText = scores;
-//UN CERCLE ROUGE INDIQUE QUEL JOUEUR JOUE
-playerOneP.innerText ='PLAYER 1 \ud83d\udd34'
-//PLAYER1 PEUT JOUER MAIS PAS PLAYER2
-
-});
-
-//AU CLIC SUR ROLLDICE PAR PLAYER 1
-rolldice.addEventListener('click', function() {
-//LE DE DONNE UNE VALEUR ALEATOIRE ENTRE 1 ET 6
-    let min= 1; 
-    let max= 7; 
-    let random = Math.floor(Math.random()*(max - min) + min); 
-    console.log(random);
- ;
-    //alert(random);
+function switchPlayer(active, playerOne, playerTwo) { 
+  // Change le joueur actif
+    if (active === playerOne) {
+    playerTwoP.innerText ='PLAYER 2 \ud83d\udd34';
+    playerOneP.innerText ='PLAYER 1';
+    currentTwo.disabled = false;
+    currentOne.disabled = true;
+    roundOne = 0;
+    roundTwo = 0;
+    currentOne.innerText = roundOne;
+    currentTwo.innerText = roundTwo;
+    roundTwo += random;
+    currentTwo.innerText = roundTwo;
+  } else {
+    playerTwoP.innerText ='PLAYER 2 ';
+    playerOneP.innerText ='PLAYER 1 \ud83d\udd34';
+    currentTwo.disabled = true;
+    currentOne.disabled = false;
+    roundOne = 0;
+    roundTwo = 0;
+    currentOne.innerText = roundOne;
+    currentTwo.innerText = roundTwo;
     
-//affichage dé /
-  let diceFace = document.createElement("IMG");
-diceFace.setAttribute("src","img/dice"+random+".png")
-document.getElementById('objDiceImg').appendChild(diceFace);
+  }
+};
 
-//le score est ajouté à 
+function newgame() {
+  console.log('ng cliqué');
+  scoreinit(); 
+  currentOne.innerText = roundOne;
+  currentTwo.innerText = roundTwo;
+  globalOne.innerText = scoreGlobalOne;
+  globalTwo.innerText = scoreGlobalTwo;
+  playerOneP.innerText ='PLAYER 1 \ud83d\udd34';
+  currentTwo.disabled = true;
+};
 
+function rolldice (){
+  let random = Math.floor(Math.random() * 6) + 1; 
+  console.log("roll cliqué", random)
+//diceFace
+  diceFace.setAttribute("src","img/dice"+random+".png")
+  diceFace.style.width = "60px";
+  diceFace.style.height="60px";
+  diceImg.appendChild(diceFace);
+//random add current
+  if (active === playerOne) {
+    roundOne += random;
+    currentOne.innerText = roundOne;
+  } else {
+    roundTwo += random;
+    currentTwo.innerText = roundTwo;
+  };
+// if random = 1 current = 0  and change active player
+  if (random ===1) {
+    switchPlayer();
+  };
+};
 
+// si joueur 1 clique sur hold score = score + current et joueur 2 joue
+function hold() {
+  console.log('hold cliqué')
+  if (active === playerOne) {
+    scoreGlobalOne += roundOne;
+    globalOne.innerText = scoreGlobalOne;
+    roundOne = 0;
+    currentOne.innerText = 0;
+  } else {
+    scoreGlobalTwo += roundTwo;
+    globalTwo.innerText = scoreglobalTwo;
+    roundTwo = 0;
+    currentTwo.innerText = 0;
+  };
+  switchPlayer();
+};  
 
-});
+newgameBtn.addEventListener('click', newgame);
+rollBtn.addEventListener('click', rolldice);
+holdBtn.addEventListener ('click', hold );
 
-/*rolldice.addEventListener('click', function() {
-  document.getElementById('objDiceImg').removeChild(diceFace); 
-});*/
